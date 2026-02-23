3fe5bbd1e132fba66b25b6aa03bf25a6e94c6a21
tomaszaba <tomas.zaba@outlook.com>
2026-02-23 15:18:40 +0200

    add larc into main script

--- script.py
+++ script.py
@@ -5,15 +5,21 @@
 
 ## ---- Decompose admission data on acute malnutrition -------------------------
 
+
 exec(open("scripts/decompose-admissions-amn.py").read())
+
 
 ## ---- Decompose admission data on childhood diseases -------------------------
 
+
 exec(open(file="scripts/decompose_diseases_admissions.py").read())
+
 
 ## ---- Analyse the trend component --------------------------------------------
 
-exec(open("scripts/seasonal-arc.py").read())
+
+exec(open("scripts/seasonal_arc.py").read())
+exec(open("scripts/local_average_rate_of_change.py").read())
 
 
 # ============================== End of Workflow ===============================
